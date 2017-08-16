acef2e93e6eeb51794e1769aa6780cc58cd21944
hanyingzhong <2655942208@qq.com>
2017-08-16 12:15:34 +0800

    Create Note.java

---
+++ mybatis/src/com/mybatis/test/Note.java
@@ -0,0 +1,29 @@
+package com.mybatis.test;
+
+import java.util.Date;
+
+/**
+ * @author turbo
+ *
+ *         2016年10月23日
+ */
+public class Note {
+	private int id;
+	private Date date;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+}
